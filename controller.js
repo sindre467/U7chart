38a5b50da7fe8ca4b147104fce8a4022ee9c8db5
sindre467 <72790639+sindre467@users.noreply.github.com>
2021-06-27 00:18:22 +0200

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,41 @@
+
+function selectB(barNo)
+ {
+     if(barNo === chosenBar){
+      chosenBar = "";
+      } else {
+      chosenBar = barNo; 
+      }   
+      unavailableBars = "";
+      show();
+}
+
+function removeB() 
+{
+      numbers.splice(chosenBar -1,1);
+      chosenBar = "" && numbers != inputValue|| '';
+      unavailableBars = 'disabled';
+      show();
+}
+
+function addB() 
+{
+     if(inputValue >= 1 && inputValue <= 10) {
+      error = '';
+      numbers.push(parseInt(inputValue));
+      } else { error ="error mellom 1-10";                               
+     }
+      show();
+}
+
+function  editB()
+{
+     if((inputValue >= 10 && inputValue <= 1) || inputValue <= 10) { 
+      numbers[chosenBar - 1] = inputValue;
+      inputValue = "";
+      error = "";
+     } else 
+     {error = '1-10';}
+
+      show();
+}
